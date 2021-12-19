5c2245ef969e49de7e4a41fd661d3aa94e2f1f2d
Your Name <you@example.com>
2021-12-19 23:02:54 +0900

    Middelware concept

--- src/main.js
+++ src/main.js
@@ -3,13 +3,31 @@
 /* eslint-disable no-console */
 
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
 const PORT = 5000
 
-app.use('/', (req, res) => {
-  res.send('Hello, express!')
+app.use('/', async (req, res, next) => {
+  console.log('Middelware 1')
+
+  const fileContent = await fs.promises.readFile('.gitignore')
+
+  const requestedAt = new Date()
+  // @ts-ignore
+  req.requestedAt = requestedAt
+  // @ts-ignore
+  req.fileContent = fileContent
+  next()
+})
+
+/* 수많은 middleware들 있다고 가정 */
+
+app.use((req, res) => {
+  console.log('Middelware 2')
+  // @ts-ignore
+  res.send(`Requested at ${req.requestedAt}, ${req.fileContent}`)
 })
 
 app.listen(PORT, () => {
